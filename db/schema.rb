040793bf50b1b02c09c07a423b691b8ba4e852f4
Rian Breno Polonini <rianbrenopolonini@gmail.com>
2021-10-10 13:48:00 -0300

    Scaffold Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_164352) do
+ActiveRecord::Schema.define(version: 2021_10_10_164518) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2021_10_10_164352) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "status"
+    t.integer "user_id", null: false
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +42,6 @@ ActiveRecord::Schema.define(version: 2021_10_10_164352) do
   end
 
   add_foreign_key "lists", "users"
+  add_foreign_key "tasks", "lists"
+  add_foreign_key "tasks", "users"
 end
