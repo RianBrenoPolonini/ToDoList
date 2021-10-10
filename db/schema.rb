d56f2de91f8e4674cf8a7467437c537639dccf36
Rian Breno Polonini <rianbrenopolonini@gmail.com>
2021-10-10 13:45:01 -0300

    Scaffold List

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_164009) do
+ActiveRecord::Schema.define(version: 2021_10_10_164352) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_10_10_164009) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "lists", "users"
 end
