af58ddc97567efe6398514088ac0a4d99758eefa
Rian Breno Polonini <rianbrenopolonini@gmail.com>
2021-10-11 02:16:42 -0300

    Creating User Registration Page

--- db/migrate/20211010164352_create_lists.rb
+++ db/migrate/20211010164352_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[6.1]
   def change
     create_table :lists do |t|
-      t.string :name
+      t.string :name, null:false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/migrate/20211010164518_create_tasks.rb
+++ db/migrate/20211010164518_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.string :description
+      t.string :name, null:false
+      t.string :description, default: ''
       t.boolean :status
       t.references :user, null: false, foreign_key: true
       t.references :list, null: false, foreign_key: true
